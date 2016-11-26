00836331a278cb0c9114e80410c8fcb7b7e8aad5
Amrit Twanabasu <amrit.cas@gmail.com>
2016-11-26 18:50:05 +0545

    Update app colors

--- app/src/main/java/np/edu/ku/kurc/posts/PostActivity.java
+++ app/src/main/java/np/edu/ku/kurc/posts/PostActivity.java
@@ -8,11 +8,11 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.v4.content.ContextCompat;
+import android.support.v4.content.res.ResourcesCompat;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.graphics.Palette;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.MenuItem;
 import android.widget.ImageView;
 
@@ -153,18 +153,22 @@ public class PostActivity extends AppCompatActivity {
             @Override
             public void onGenerated(Palette palette) {
                 Palette.Swatch vibrant = palette.getVibrantSwatch();
-                if (vibrant != null) {
-                    collapsingToolbar.setCollapsedTitleTextColor(vibrant.getTitleTextColor());
-                }
 
                 int mutedColor = palette.getMutedColor(ContextCompat.getColor(getApplicationContext(), R.color.colorPrimary));
                 int mutedDarkColor = palette.getDarkMutedColor(ContextCompat.getColor(getApplicationContext(), R.color.colorPrimaryDark));
 
-                collapsingToolbar.setContentScrimColor(mutedColor);
-                collapsingToolbar.setStatusBarScrimColor(mutedDarkColor);
+                if(mutedColor != ResourcesCompat.getColor(getResources(),R.color.colorPrimary,null) &&
+                mutedDarkColor != ResourcesCompat.getColor(getResources(),R.color.colorPrimaryDark,null)) {
+                    if (vibrant != null) {
+                        collapsingToolbar.setCollapsedTitleTextColor(vibrant.getTitleTextColor());
+                    }
 
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-                    getWindow().setNavigationBarColor(mutedDarkColor);
+                    collapsingToolbar.setContentScrimColor(mutedColor);
+                    collapsingToolbar.setStatusBarScrimColor(mutedDarkColor);
+
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                        getWindow().setNavigationBarColor(mutedDarkColor);
+                    }
                 }
             }
         });
